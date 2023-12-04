b334dadff70f91c68c34571d2fe5dd89d2ab233a
rbalazs89 <ritzinger.balazs@gmail.com>
2023-12-04 11:26:57 +0100

    day 4 part 2 done

--- day-4/src/Main.java
+++ day-4/src/Main.java
@@ -9,17 +9,19 @@ public class Main {
     public static void main(String[] args) {
         List<String> myInput = readFile("src/input2.txt");
         System.out.println(part1(myInput));
+        System.out.println(part2(myInput));
     }
     public static int part1(List<String> input){
         ArrayList<Integer> winningNumbers = new ArrayList<>();
         ArrayList<Integer> myNumbers = new ArrayList<>();
         int solution = 0;
+        int fix = 2;
         for (String s : input) {
             int hits = 0;
-            for (int j = 10; j < 40; j = j + 3) {
+            for (int j = 10 - fix; j < 40 - fix; j = j + 3) {
                 winningNumbers.add(Integer.valueOf(s.substring(j, j + 2).replaceAll("\\s", "")));
             }
-            for (int k = 42; k < s.length(); k = k + 3) {
+            for (int k = 42 - fix; k < s.length(); k = k + 3) {
                 myNumbers.add(Integer.valueOf(s.substring(k, k + 2).replaceAll("\\s", "")));
             }
             for (Integer winningNumber : winningNumbers) {
@@ -36,6 +38,51 @@ public class Main {
 
         return solution;
     }
+
+    public static int part2(List<String> input){
+        ArrayList<Integer> winningNumbers = new ArrayList<>();
+        ArrayList<Integer> myNumbers = new ArrayList<>();
+        int[] copies = new int[input.size()];
+        int solution = 0;
+        int i = 0;
+        for (String s : input) {
+            int hits = 0;
+            for (int j = 10; j < 40; j = j + 3) {
+                winningNumbers.add(Integer.valueOf(s.substring(j, j + 2).replaceAll("\\s", "")));
+            }
+            for (int k = 42; k < s.length(); k = k + 3) {
+                myNumbers.add(Integer.valueOf(s.substring(k, k + 2).replaceAll("\\s", "")));
+            }
+            for (Integer winningNumber : winningNumbers) {
+                if (myNumbers.contains(winningNumber)) {
+                    hits++;
+                }
+            }
+
+            for(int n = 0; n < copies[i] + 1; n ++){
+                for(int l = 0; l < Math.min(hits, input.size() - 1); l ++){
+                    copies[Math.min(l + i + 1, input.size() - 1)] += 1;
+                }
+            }
+
+            for(int kk = 0; kk < copies.length; kk ++){
+                System.out.println( kk + 1 + " " + copies[kk]);
+            }
+
+            winningNumbers.clear();
+            myNumbers.clear();
+            i++;
+        }
+        for(int m = 0; m < input.size(); m ++){
+            solution = solution + copies[m];
+        }
+        for(int kk = 0; kk < copies.length; kk ++){
+            System.out.println( kk + " " + copies[kk]);
+        }
+        return solution + input.size();
+    }
+
+
 
     public static List<String> readFile(String file) {
         Path filePath = Paths.get(file);

1b86ed1148a4da5187fb4da97c49d42d21d82722
rbalazs89 <ritzinger.balazs@gmail.com>
2023-12-05 18:57:03 +0100

    part 2 done, works, but runtime is ~5 min, TODO need to improve

--- day-5/src/Main.java
+++ day-5/src/Main.java
@@ -3,9 +3,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-
-import static java.lang.Character.isDigit;
 
 public class Main {
 
@@ -18,82 +15,17 @@ public class Main {
     static ArrayList<Long> humidityToLocation = new ArrayList<>();
     static ArrayList<Long> seeds = new ArrayList<>();
 
-    public static void main(String[] args) throws {
+    public static void main(String[] args) {
+        processFile(); //gets the globally declared arraylists.
 
-        List<String> myInput = readFile("src/input2.txt");
-        processFile();
-        //System.out.println(part1(myInput));
-        System.out.println(test(myInput));
+        //System.out.println(part1());
+        System.out.println(part2());
     }
 
-    public static long part1 (List<String> input){
-
-        for(int i = 0 ; i < seeds.size(); i ++){
-            for(int j = 0 ; j < seedToSoil.size(); j = j + 3){
-                if(seeds.get(i) < seedToSoil.get(j + 1) + seedToSoil.get(j + 2) && seeds.get(i) > seedToSoil.get(j + 1)){
-                    seeds.set(i, (seeds.get(i) - seedToSoil.get(j + 1)) + seedToSoil.get(j));
-                }
-            }
-
-            // For soilToFertilizer ArrayList
-            for (int j = 0; j < soilToFertilizer.size(); j = j + 3) {
-                if (seeds.get(i) < soilToFertilizer.get(j + 1) + soilToFertilizer.get(j + 2)
-                        && seeds.get(i) > soilToFertilizer.get(j + 1)) {
-                    seeds.set(i, (seeds.get(i) - soilToFertilizer.get(j + 1)) + soilToFertilizer.get(j));
-                }
-            }
-
-            // For fertilizerToWater ArrayList
-            for (int j = 0; j < fertilizerToWater.size(); j = j + 3) {
-                if (seeds.get(i) < fertilizerToWater.get(j + 1) + fertilizerToWater.get(j + 2)
-                        && seeds.get(i) > fertilizerToWater.get(j + 1)) {
-                    seeds.set(i, (seeds.get(i) - fertilizerToWater.get(j + 1)) + fertilizerToWater.get(j));
-                }
-            }
-
-            // For waterToLight ArrayList
-            for (int j = 0; j < waterToLight.size(); j = j + 3) {
-                if (seeds.get(i) < waterToLight.get(j + 1) + waterToLight.get(j + 2)
-                        && seeds.get(i) > waterToLight.get(j + 1)) {
-                    seeds.set(i, (seeds.get(i) - waterToLight.get(j + 1)) + waterToLight.get(j));
-                }
-            }
-
-            // For lightToTemperature ArrayList
-            for (int j = 0; j < lightToTemperature.size(); j = j + 3) {
-                if (seeds.get(i) < lightToTemperature.get(j + 1) + lightToTemperature.get(j + 2)
-                        && seeds.get(i) > lightToTemperature.get(j + 1)) {
-                    seeds.set(i, (seeds.get(i) - lightToTemperature.get(j + 1)) + lightToTemperature.get(j));
-                }
-            }
-
-            // For temperatureToHumidity ArrayList
-            for (int j = 0; j < temperatureToHumidity.size(); j = j + 3) {
-                if (seeds.get(i) < temperatureToHumidity.get(j + 1) + temperatureToHumidity.get(j + 2)
-                        && seeds.get(i) > temperatureToHumidity.get(j + 1)) {
-                    seeds.set(i, (seeds.get(i) - temperatureToHumidity.get(j + 1)) + temperatureToHumidity.get(j));
-                }
-            }
-
-            // For humidityToLocation ArrayList
-            for (int j = 0; j < humidityToLocation.size(); j = j + 3) {
-                if (seeds.get(i) < humidityToLocation.get(j + 1) + humidityToLocation.get(j + 2)
-                        && seeds.get(i) > humidityToLocation.get(j + 1)) {
-                    seeds.set(i, (seeds.get(i) - humidityToLocation.get(j + 1)) + humidityToLocation.get(j));
-                }
-            }
-        }
-
-        Collections.sort(seeds);
-        return seeds.get(0);
-    }
-
-    public static long test (List<String> input){
+    public static long part1 (){
 
         //List<Long> seeds = Arrays.asList(new Long[]{79L, 14L, 55L, 13L});
         //List<Long> seeds = Arrays.asList(new Long[]{14L});
-
-        System.out.println(fertilizerToWater);
 
         for(int i = 0 ; i < seeds.size(); i ++){
             for(int j = 0 ; j < seedToSoil.size(); j = j + 3){
@@ -160,6 +92,88 @@ public class Main {
 
         Collections.sort(seeds);
         return seeds.get(0);
+    }
+
+    public static long part2 (){
+
+        Long solution = Long.MAX_VALUE;
+
+        for (int m = 0; m < seeds.size() ; m = m + 2) {
+
+            Long rangeMin = seeds.get(m);
+            Long rangeMax = seeds.get(m) + seeds.get(m+1);
+            while (rangeMin < rangeMax) {
+                
+                Long processedNumber = rangeMin;
+                rangeMin ++;
+
+                for (int j = 0; j < seedToSoil.size(); j = j + 3) {
+                    if (processedNumber < seedToSoil.get(j + 1) + seedToSoil.get(j + 2) && processedNumber >= seedToSoil.get(j + 1)) {
+                        processedNumber = (processedNumber - seedToSoil.get(j + 1)) + seedToSoil.get(j);
+                        break;
+                    }
+                }
+
+                // For soilToFertilizer ArrayList
+                for (int j = 0; j < soilToFertilizer.size(); j = j + 3) {
+                    if (processedNumber < soilToFertilizer.get(j + 1) + soilToFertilizer.get(j + 2)
+                            && processedNumber >= soilToFertilizer.get(j + 1)) {
+                        processedNumber = (processedNumber - soilToFertilizer.get(j + 1)) + soilToFertilizer.get(j);
+                        break;
+                    }
+                }
+
+                // For fertilizerToWater ArrayList
+                for (int j = 0; j < fertilizerToWater.size(); j = j + 3) {
+                    if (processedNumber < fertilizerToWater.get(j + 1) + fertilizerToWater.get(j + 2)
+                            && processedNumber >= fertilizerToWater.get(j + 1)) {
+                        processedNumber = (processedNumber - fertilizerToWater.get(j + 1)) + fertilizerToWater.get(j);
+                        break;
+                    }
+                }
+
+                // For waterToLight ArrayList
+                for (int j = 0; j < waterToLight.size(); j = j + 3) {
+                    if (processedNumber < waterToLight.get(j + 1) + waterToLight.get(j + 2)
+                            && processedNumber >= waterToLight.get(j + 1)) {
+                        processedNumber = (processedNumber - waterToLight.get(j + 1)) + waterToLight.get(j);
+                        break;
+                    }
+                }
+
+                // For lightToTemperature ArrayList
+                for (int j = 0; j < lightToTemperature.size(); j = j + 3) {
+                    if (processedNumber < lightToTemperature.get(j + 1) + lightToTemperature.get(j + 2)
+                            && processedNumber >= lightToTemperature.get(j + 1)) {
+                        processedNumber = (processedNumber - lightToTemperature.get(j + 1)) + lightToTemperature.get(j);
+                        break;
+                    }
+                }
+
+                // For temperatureToHumidity ArrayList
+                for (int j = 0; j < temperatureToHumidity.size(); j = j + 3) {
+                    if (processedNumber < temperatureToHumidity.get(j + 1) + temperatureToHumidity.get(j + 2)
+                            && processedNumber >= temperatureToHumidity.get(j + 1)) {
+                        processedNumber = (processedNumber - temperatureToHumidity.get(j + 1)) + temperatureToHumidity.get(j);
+                        break;
+                    }
+                }
+
+                // For humidityToLocation ArrayList
+                for (int j = 0; j < humidityToLocation.size(); j = j + 3) {
+                    if (processedNumber < humidityToLocation.get(j + 1) + humidityToLocation.get(j + 2)
+                                && processedNumber >= humidityToLocation.get(j + 1)) {
+                            processedNumber = (processedNumber - humidityToLocation.get(j + 1)) + humidityToLocation.get(j);
+                            break;
+                    }
+                }
+                if(solution > processedNumber) {
+                    solution = processedNumber;
+                }
+            }
+        }
+        
+        return solution;
     }
 
     public static List<String> readFile(String file) {

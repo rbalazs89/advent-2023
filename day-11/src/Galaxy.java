f9cb26f2cf553b1cf38e7c36bb8f6fbf7f81e60a
rbalazs89 <ritzinger.balazs@gmail.com>
2023-12-18 06:06:51 +0100

    day 11 done

--- day-11/src/Galaxy.java
+++ day-11/src/Galaxy.java
@@ -2,8 +2,13 @@ public class Galaxy {
     int x;
     int y;
 
+    int enlargedX;
+    int enlargedY;
+
     public Galaxy(int x, int y){
         this.x = x;
         this.y = y;
+        enlargedX = x;
+        enlargedY = y;
     }
 }
--- day-11/src/Main.java
+++ day-11/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         List<String> input = readFile("src/input2.txt");
         System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 
     public static int part1(List<String> input){
@@ -77,6 +78,87 @@ public class Main {
                 result += Math.abs(galaxies.get(i).x - galaxies.get(j).x) + Math.abs(galaxies.get(i).y - galaxies.get(j).y);
             }
         }
+
+
+        return result/2;
+    }
+
+    public static long part2(List<String> input){
+
+        //PROCESS INPUT FILE:
+        ArrayList<Galaxy> galaxies = new ArrayList<>();
+        int sizeX = input.get(0).length();
+        int sizeY = input.size();
+        for(int i = 0; i < sizeY; i ++){
+            for (int j = 0; j < sizeX; j++) {
+                if(input.get(i).charAt(j) == '#'){
+                    galaxies.add(new Galaxy(j,i));
+                }
+            }
+        }
+
+        //COSMIC EXPANSION:
+        //Vertical expansion:
+        int yEnlargedExpansion = 999999;
+        int yExpansion = 0;
+        for(int i = 0; i < sizeY; i ++){
+
+            int tempCounter = 0;
+            for (int j = 0; j < sizeX; j++) {
+                if(input.get(i).charAt(j) != '#'){
+                    tempCounter++;
+                }
+            }
+
+            if (tempCounter == sizeX){
+
+                yExpansion++;
+
+                for (int k = 0; k < galaxies.size(); k++) {
+                    if(galaxies.get(k).y >= i + yExpansion){
+                        galaxies.get(k).y ++;
+                        galaxies.get(k).enlargedY = galaxies.get(k).enlargedY + yEnlargedExpansion;
+                    }
+                }
+            }
+        }
+
+        //Horizontal expansion:
+        int xExpansion = 0;
+        int xEnlargedExpansion = 999999;
+        for(int i = 0; i < sizeX; i ++){
+
+            int tempCounter = 0;
+            for (int j = 0; j < sizeY; j++) {
+                if(input.get(j).charAt(i) != '#'){
+                    tempCounter++;
+                }
+            }
+
+            if (tempCounter == sizeY){
+
+                xExpansion++;
+                for (int k = 0; k < galaxies.size(); k++) {
+                    if(galaxies.get(k).x >= i + xExpansion){
+                        galaxies.get(k).x ++;
+                        galaxies.get(k).enlargedX = galaxies.get(k).enlargedX + xEnlargedExpansion;
+                    }
+                }
+            }
+        }
+
+        long result = 0;
+        //CALCULATING DISTANCES
+        for (int i = 0; i < galaxies.size(); i++) {
+            for (int j = 0; j < galaxies.size(); j++) {
+                result += Math.abs(galaxies.get(i).enlargedX - galaxies.get(j).enlargedX) + Math.abs(galaxies.get(i).enlargedY - galaxies.get(j).enlargedY);
+            }
+        }
+
+        for (int i = 0; i < galaxies.size(); i++) {
+            System.out.println(galaxies.get(i).enlargedY + " " +galaxies.get(i).enlargedX);
+        }
+
 
         return result/2;
     }
